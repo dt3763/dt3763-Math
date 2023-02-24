1f39b506b65a4ac562ff12e3f8abe22f14c4544f
dt3763 <dt3763@gmail.com>
2023-02-24 15:51:03 +0000

    math

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,10 @@ public class Lab {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return 0;
+       
+                        
+         return Math.pow(a, b);
+        
     }
+    
 }
